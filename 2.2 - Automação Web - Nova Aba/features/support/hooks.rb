d96741ecb3d9f42e865643cd1ddf6e5739d969bf
Luan Linares <luanlnrs@gmail.com>
2020-03-03 17:50:10 -0300

    Open Challenge 2.2

---
+++ 2.2 - Automação Web - Nova Aba/features/support/hooks.rb
@@ -0,0 +1,8 @@
+Before do
+    visit "https://the-internet.herokuapp.com/windows" 
+    Capybara.page.driver.browser.manage.window.maximize
+end
+
+After do  
+    Capybara.current_session.driver.quit
+end

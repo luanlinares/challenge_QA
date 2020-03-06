bcdabeffdd36e17bc001ad8bb57e7099e2372638
Luan Linares <luanlnrs@gmail.com>
2020-03-06 10:09:21 -0300

    Ajustes gerais

--- 2.3 - Automação - API/features/pages/post_page.rb
+++ 2.3 - Automação - API/features/pages/post_page.rb
@@ -3,7 +3,7 @@ class Apipost
     def api_post
         HTTParty.post(
              "http://jsonplaceholder.typicode.com/posts",
-            body:{title: "creditas",body: "challenge creditas", userId:"101"}.to_json,
+            body:{title: "creditas",body: "teste creditas", userId:"2"}.to_json,
            headers:{
           "Content-Type" => "application/jason",
            },
--- 2.3 - Automação - API/features/step_definitions/post.rb
+++ 2.3 - Automação - API/features/step_definitions/post.rb
@@ -7,5 +7,5 @@ end
 Então("a API deve retornar o codigo {int}") do |int|
     expect(@resultado.code).to eq   201
     puts "Código de retorno é #{@resultado.code}"
-    expect(@resultado["body"]).to eq "challenge creditas"
+    expect(@resultado["body"]).to eq "teste creditas"
 end
